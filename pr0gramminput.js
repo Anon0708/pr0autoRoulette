0a3691ae2d98321d13a3200881b11923a3c875e7
Anon0708 <hpeter689@yahoo.de>
2021-03-27 19:19:17 +0100

    Initial Commit

---
+++ pr0gramminput.js
@@ -0,0 +1,40 @@
+let bet_input = 0;
+let history_length = 0;
+let max_bet = 64;
+setInterval(async () => {
+    history_length = document.getElementById("history").children
+    if(!document.getElementById('spin').disabled && temp1.getFields().red == undefined) {
+        if(history_length.length != 0) {
+            let history = document.getElementById("history").children[0].innerText
+            let index_start = history.indexOf("Win");
+            let index_last = history.indexOf("(");
+            let res = history.substring(index_start+4, index_last);
+            let win_int = parseInt(res)
+
+            index_start = history.indexOf("Bet");
+            index_last = history.indexOf("(");
+            res = history.substring(index_start+4, index_last);
+            let bet_int = parseInt(res)
+
+            if(win_int > 0) {
+                bet_input = 1
+            }
+            else {
+                if(bet_input < max_bet) {
+                    bet_input  = bet_int * 2
+                }
+                else {
+                    bet_input = 1
+                }
+            }
+            document.getElementById("bet").value = bet_input
+        }
+        
+        
+        while (temp1.getFields().red == undefined) {
+            await temp1.placeChip({x: "lol"});
+            document.getElementById("spin").click()
+        }
+    }
+}, 500)
+    
